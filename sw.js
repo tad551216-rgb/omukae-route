6a740dee8f5f8fa77e96b6bd1f8b358afc4a584e
tad551216-rgb <tad551216@gmail.com>
2026-07-12 07:24:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'omukae-techo-v33';
+const CACHE = 'omukae-techo-v34';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
